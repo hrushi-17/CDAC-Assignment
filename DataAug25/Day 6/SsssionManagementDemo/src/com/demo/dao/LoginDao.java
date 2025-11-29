daac0adb4635d5e71f94aec5c4e70a33a528aebc
hrushikesh chothe <121479407+hrushi-17@users.noreply.github.com>
2025-11-29 20:27:56 +0530

    Delete DataAug25/Day 6 directory

--- DataAug25/Day 6/SsssionManagementDemo/src/com/demo/dao/LoginDao.java
+++
@@ -1,9 +0,0 @@
-package com.demo.dao;
-
-import com.demo.beans.MyUser;
-
-public interface LoginDao {
-
-	MyUser checkUser(String uname, String pass);
-
-}
--- DataAug25/Day 6/SsssionManagementDemo/src/com/demo/service/LoginService.java
+++
@@ -1,9 +0,0 @@
-package com.demo.service;
-
-import com.demo.beans.MyUser;
-
-public interface LoginService {
-
-	MyUser validateUser(String uname, String pass);
-
-}
