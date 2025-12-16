05f22339c89f662a00e26c7a4f09de2575be38f7
hrushikesh chothe <121479407+hrushi-17@users.noreply.github.com>
2025-12-16 20:01:00 +0530

    Add files via upload

---
+++ DotNet/Day 01/01_NETDemo/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_NETDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 40; i++)
+            {
+                int count = 0;
+                for (int j = 2; j < i / 2; j++)
+                {
+                    if (i % j == 0)
+                    {
+                        count++;
+                    }
+
+
+                }
+                if (count > 0)
+                    Console.WriteLine("The Number is NOT Prime -" + i);
+                else
+                    Console.WriteLine("Is Prime -" + i);
+
+            }
+
+        }
+    }
+}
---
+++ DotNet/Day 01/01_StartPattern/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_StartPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 5; // controls the size of the diamond (half height)
+
+            // upper half
+            for (int i = 1; i <= n; i++)
+            {
+                // leading spaces
+                for (int j = i; j < n; j++)
+                    Console.Write(" ");
+
+                // stars and spaces inside
+                for (int j = 1; j <= (2 * i - 1); j++)
+                {
+                    if (j == 1 || j == (2 * i - 1))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+
+            // lower half
+            for (int i = n - 1; i >= 1; i--)
+            {
+                // leading spaces
+                for (int j = n; j > i; j--)
+                    Console.Write(" ");
+
+                // stars and spaces inside
+                for (int j = 1; j <= (2 * i - 1); j++)
+                {
+                    if (j == 1 || j == (2 * i - 1))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+
+                Console.WriteLine();
+            }
+
+
+        }
+    }
+}
+
+
+
