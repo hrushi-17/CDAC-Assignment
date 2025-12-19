ce934293cf60235260ebad65f592f1d9af802a63
hrushikesh chothe <121479407+hrushi-17@users.noreply.github.com>
2025-12-19 19:43:01 +0530

    Add files via upload

---
+++ DotNet/Day 04/17Demo_WinForm/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _17Demo_WinForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            Button button = new Button();
+            button.Text = "OK";
+
+            button.Click += new EventHandler(button1_Click);
+
+            this.Controls.Add(button);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You clicked a button!");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("You clicked a label!");
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+    }
+}
