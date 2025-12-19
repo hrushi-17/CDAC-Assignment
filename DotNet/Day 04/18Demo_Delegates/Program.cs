1fb740b079a99c2b903b500526140528ce40a696
hrushikesh chothe <121479407+hrushi-17@users.noreply.github.com>
2025-12-19 19:45:03 +0530

    Add files via upload

---
+++ DotNet/Day 04/18Demo_Delegates/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace _18Demo_Delegates
+{
+
+    // A delegate is a Type Safe Function Pointer
+    // - Event Handlers
+
+    // Syntax : 
+    // (accessModifier) delegate (Return-Type of Method) (Delegate NAme)(Parameter-Type(s));
+
+    public delegate void MyDelegate();
+    public delegate void MyDelegate2(String str);
+    public delegate int AddDelegate(int m, int n);
+    public delegate int MySquareDelegate(int m);
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Cmath mn = new Cmath();
+            AddDelegate mydel = new AddDelegate(mn.add);
+            Console.WriteLine("Enter the 1st Number-> ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the 2nd Number-> ");
+            int m = Convert.ToInt32(Console.ReadLine());
+
+            int addProduct = mydel(m, n);
+
+            MySquareDelegate sq = new MySquareDelegate(mn.square);
+            int sq1 = sq(n);
+            int sq2 = sq(m);
+
+            Welcome wc = new Welcome();
+            MyDelegate m1 = new MyDelegate(wc.Hello);
+            MyDelegate2 m2 = new MyDelegate2(wc.Greet);
+
+            m1.Invoke();
+            m2.Invoke("Hrushi");
+
+            Console.WriteLine($"THe Addition of Numbers is-> {addProduct}  ");
+            Console.WriteLine($"......whereas the Square of 1st Number is {sq1} and 2nd Number is {sq2}");
+
+
+        }
+    }
+    public class Cmath
+    {
+        public int add(int x, int y)
+        {
+            return x + y;
+        }
+        public int square(int x)
+        {
+            return x * x;
+        }
+    }
+    public class Welcome
+    {
+        public void Hello()
+        {
+            Console.WriteLine("Batman");
+        }
+
+        public void Greet(string name)
+        {
+            Console.WriteLine($"Welcome Avenger...{name}");
+        }
+    }
+}
---
+++ DotNet/Day 04/20Demo_MultiCastDelegate/Program.cs
@@ -0,0 +1,70 @@
+﻿namespace _20Demo_MultiCastDelegate
+{
+    public delegate void MyEventHandler();
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Is Guest arrived?");
+            string isGuestArrived = Console.ReadLine();
+
+            CollegeAnnualEvent obj = new CollegeAnnualEvent();
+            // handler is our Muti-cast Delegate
+            MyEventHandler handler = obj.Welcome;
+            handler += obj.WelSpeech;
+
+            if(isGuestArrived == "y")
+            {
+                handler += obj.GeustSpeech;
+            }
+            else
+            {
+                handler -= obj.GeustSpeech;
+            }
+
+            handler += obj.Dance;
+            handler += obj.Dinner;
+            handler += obj.GoodBye;
+            handler += obj.EndEvent;
+
+            handler();
+
+            //MyEventHandler handler1 = new MyEventHandler(obj.WelCome);
+            //MyEventHandler handler2 = obj.WelSpeech;
+            //MyEventHandler handler3 += obj.GuestSpeech;
+        }
+    }
+
+    public class CollegeAnnualEvent { 
+    
+        public void Welcome()
+        {
+            Console.WriteLine("Wel-come Everyone!!");
+        }
+
+        public void WelSpeech()
+        {
+            Console.WriteLine("Host: blah blah blah blah!!");
+        }
+        public void GeustSpeech()
+        {
+            Console.WriteLine("Guest: blah blah blah blah!!");
+        }
+        public void Dance()
+        {
+            Console.WriteLine("Oh GOD, we have have to go through this!!");
+        }
+        public void Dinner()
+        {
+            Console.WriteLine("We insist everyone , please enjoy dinner...!!");
+        }
+        public void GoodBye()
+        {
+            Console.WriteLine("Bye bye.. please ghar jao...!!");
+        }
+        public void EndEvent()
+        {
+            Console.WriteLine("Pack UP...!!");
+        }
+    }
+}
