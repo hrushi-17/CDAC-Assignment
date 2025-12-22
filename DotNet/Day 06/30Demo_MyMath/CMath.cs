24a932c39eaee93a897e5111065613edb7e9d126
hrushikesh chothe <121479407+hrushi-17@users.noreply.github.com>
2025-12-22 17:52:22 +0530

    Add files via upload

---
+++ DotNet/Day 06/30Demo_MyMath/CMath.cs
@@ -0,0 +1,15 @@
+﻿namespace _30Demo_MyMath
+{
+    public class CMath
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+    }
+}
