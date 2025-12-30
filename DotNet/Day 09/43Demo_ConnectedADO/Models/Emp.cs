94477f2c7d33d050909fa289a7379b9e10e14169
hrushikesh chothe <121479407+hrushi-17@users.noreply.github.com>
2025-12-30 15:45:53 +0530

    Add files via upload

---
+++ DotNet/Day 09/43Demo_ConnectedADO/Models/Emp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _43Demo_ConnectedADO.Models
+{
+    // Models Layer
+    // Buissness Entities Layer
+    // Plain CLR Object [POCO]
+    public class Emp
+    {
+        public int EId { get; set; }
+        public string? EName { get; set; }
+        public string? EAddress { get; set; }
+    }
+}
